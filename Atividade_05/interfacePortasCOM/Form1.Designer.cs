2fff6b761e2a0c68777f6b590ceeea76bb1da7b4
Gaban03 <gabanvinicius724@gmail.com>
2025-03-19 22:04:13 -0300

    Comunicação com porta serial do Arduino

---
+++ Atividade_05/interfacePortasCOM/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace interfacePortasCOM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tsMenuSystem = new System.Windows.Forms.ToolStrip();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.cmbCOM = new System.Windows.Forms.ToolStripComboBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtExibirDados = new System.Windows.Forms.TextBox();
+            this.txtEnviar = new System.Windows.Forms.TextBox();
+            this.timerComunication = new System.Windows.Forms.Timer(this.components);
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.tsMenuSystem.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tsMenuSystem
+            // 
+            this.tsMenuSystem.BackColor = System.Drawing.Color.Silver;
+            this.tsMenuSystem.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
+            this.tsMenuSystem.Location = new System.Drawing.Point(0, 0);
+            this.tsMenuSystem.Name = "tsMenuSystem";
+            this.tsMenuSystem.Size = new System.Drawing.Size(1077, 25);
+            this.tsMenuSystem.TabIndex = 0;
+            this.tsMenuSystem.Text = "toolStrip1";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmbCOM});
+            this.toolStripSplitButton1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(65, 22);
+            this.toolStripSplitButton1.Text = "COM";
+            // 
+            // cmbCOM
+            // 
+            this.cmbCOM.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.cmbCOM.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCOM.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCOM.Name = "cmbCOM";
+            this.cmbCOM.Size = new System.Drawing.Size(121, 23);
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnConectar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConectar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(187, 655);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(134, 52);
+            this.btnConectar.TabIndex = 1;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = false;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnEnviar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEnviar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(749, 655);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(134, 52);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtExibirDados
+            // 
+            this.txtExibirDados.Location = new System.Drawing.Point(38, 63);
+            this.txtExibirDados.Multiline = true;
+            this.txtExibirDados.Name = "txtExibirDados";
+            this.txtExibirDados.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtExibirDados.Size = new System.Drawing.Size(472, 550);
+            this.txtExibirDados.TabIndex = 3;
+            // 
+            // txtEnviar
+            // 
+            this.txtEnviar.Location = new System.Drawing.Point(567, 63);
+            this.txtEnviar.Multiline = true;
+            this.txtEnviar.Name = "txtEnviar";
+            this.txtEnviar.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEnviar.Size = new System.Drawing.Size(472, 550);
+            this.txtEnviar.TabIndex = 4;
+            // 
+            // timerComunication
+            // 
+            this.timerComunication.Enabled = true;
+            this.timerComunication.Interval = 1000;
+            this.timerComunication.Tick += new System.EventHandler(this.timerComunication_Tick);
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 726);
+            this.Controls.Add(this.txtEnviar);
+            this.Controls.Add(this.txtExibirDados);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.tsMenuSystem);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Comunicação COM";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.tsMenuSystem.ResumeLayout(false);
+            this.tsMenuSystem.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip tsMenuSystem;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripComboBox cmbCOM;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtExibirDados;
+        private System.Windows.Forms.TextBox txtEnviar;
+        private System.Windows.Forms.Timer timerComunication;
+        private System.IO.Ports.SerialPort serialPort;
+    }
+}
+
---
+++ Atividade_05/interfacePortasCOM/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace interfacePortasCOM
+{
+    public partial class Form1 : Form
+    {
+        string RxString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void atualizaListaCOMs()
+        {
+            int i;
+            bool quantDiferente; // flag para sinalizar que a qunatidade de portas mudou
+
+            i = 0;
+            quantDiferente = false;
+
+            // se a quantidade de portas mudou
+            if (cmbCOM.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                foreach(string s in SerialPort.GetPortNames())
+                {
+                    if (cmbCOM.Items[i++].Equals(s) == false)
+                    {
+                        quantDiferente = true;
+                    }
+                }       
+            }
+            else
+            {
+                quantDiferente = true;
+            }
+
+            //Se não foi detectado diferença
+            if(quantDiferente == false)
+            {
+                return; // retorna
+            }
+
+            // Limpa comboBox
+            cmbCOM.Items.Clear();
+
+            //adiciona todas as COM disponíveis na lista
+            foreach(string s in SerialPort.GetPortNames())
+            {
+                cmbCOM.Items.Add(s);
+            }
+            //seleciona a primeira posição da lista
+            cmbCOM.SelectedIndex = -1;
+        }
+
+        private void timerComunication_Tick(object sender, EventArgs e)
+        {
+          atualizaListaCOMs();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen == false)
+            {
+                try
+                {
+                    serialPort.PortName = cmbCOM.Items[cmbCOM.SelectedIndex].ToString();
+                    serialPort.Open();
+                }
+                catch
+                {
+                    return ;
+                }
+
+                if (serialPort.IsOpen)
+                {
+                    btnConectar.Text = "Desconectar";
+                    cmbCOM.Enabled = false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    serialPort.Close();
+                    cmbCOM.Enabled = true;
+                    btnConectar.Text = "Conectar";
+                }
+                catch
+                {
+                    return;
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(serialPort.IsOpen == true) // se porta aberta
+            {
+                serialPort.Close(); // fecha a porta
+            }
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if(serialPort.IsOpen == true) // se porta está aberta
+            {
+                serialPort.Write(txtEnviar.Text); // envia o texto presente no textBox Enviar
+            }
+        }
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            RxString = serialPort.ReadExisting();
+            this.Invoke(new EventHandler(trataDadoRecebido));
+        }
+
+        private void trataDadoRecebido(object sender, EventArgs e)
+        {
+            txtExibirDados.AppendText(RxString);
+        }
+    }
+}
